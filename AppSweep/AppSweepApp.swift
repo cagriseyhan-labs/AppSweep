90598a43a31ebb3bf9413ad50399f0055ba05d93
cagriseyhan <cagriseyhan16@gmail.com>
2025-11-01 22:07:28 +0300

    Initial Commit

---
+++ AppSweep/AppSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSweepApp.swift
+//  AppSweep
+//
+//  Created by Çağrı Seyhan on 1.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
